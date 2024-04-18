8eed44def2fe8c90818880ed9414e021b82c52cb
Mustafa Kılıç <enciyomk61@gmail.com>
2024-04-18 21:52:55 +0300

    reorganized

--- src/logger.py
+++ src/logger.py
@@ -10,14 +10,13 @@ def take_screen_shot(sb):
     os.system(f"mv screenshot{count}.png {CONST_WORKSPACE}")
 
 
+
 def append_log_file(message):
-    path = f"{CONST_WORKSPACE}/log.txt"
-    if not os.path.exists(path):
-        with open(path,"w") as f:
-            f.write(f"[{time.strftime('%Y-%m-%d %H:%M:%S')}] {message}\n")
-    else:
-        with open(path,"a") as f:
-            f.write(f"[{time.strftime('%Y-%m-%d %H:%M:%S')}] {message}\n")
+    with open(f"{CONST_WORKSPACE}/log.txt", "a") as file:
+        file.write(f"{time.ctime()} - {message}\n")
+        file.close()
+
+
 
 
 
