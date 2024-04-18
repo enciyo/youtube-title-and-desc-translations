cbdebffd9862a2e59802d94cce5cb7b6af7a6ba2
Mustafa Kılıç <enciyomk61@gmail.com>
2024-04-18 21:57:00 +0300

    reorganized

--- src/logger.py
+++ src/logger.py
@@ -7,12 +7,12 @@ def take_screen_shot(sb):
     global count
     count += 1
     sb.save_screenshot(f"screenshot{count}.png")
-    os.system(f"mv screenshot{count}.png {CONST_WORKSPACE}")
+    os.system(f"mv screenshot{count}.png output")
 
 
 
 def append_log_file(message):
-    with open(f"{CONST_WORKSPACE}/log.txt", "a") as file:
+    with open(f"output/log.txt", "a") as file:
         file.write(f"{time.ctime()} - {message}\n")
         file.close()
 
