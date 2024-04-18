5df7f8042512876f2a3685b9478b70f825db3ff7
Mustafa Kılıç <enciyomk61@gmail.com>
2024-04-18 23:36:57 +0300

    reorganized

--- src/telegram_bot.py
+++ src/telegram_bot.py
@@ -1,12 +1,9 @@
 import requests
 from constants import CONST_TELEGRAM_TOKEN, CONST_TELEGRAM_CHAT_ID
 
-
 def send_photo_file(file_path: str):
     with open(file_path, "rb") as f:
-        response = requests.put(f"https://api.telegram.org/bot{CONST_TELEGRAM_TOKEN}/sendPhoto", {"chat_id": CONST_TELEGRAM_CHAT_ID}, files={"photo": file_path})
+        response = requests.post(f"https://api.telegram.org/bot{CONST_TELEGRAM_TOKEN}/sendPhoto", {"chat_id": CONST_TELEGRAM_CHAT_ID}, files={"photo": f})
         print(response.json())
-
-
 
 
