a132156bcc284de1132d232aa1c2259408866307
Mustafa Kılıç <enciyomk61@gmail.com>
2024-04-18 21:38:23 +0300

    reorganized

--- src/logger.py
+++ src/logger.py
@@ -10,6 +10,5 @@ def take_screen_shot(sb):
 
 
 def append_log_file(message):
-    with open("log.txt","a") as f:
+    with open(f"{CONST_WORKSPACE}/log.txt","a") as f:
         f.write(f"[{time.strftime('%Y-%m-%d %H:%M:%S')}] {message}\n")
-        os.system(f"mv log.txt {CONST_WORKSPACE}")
