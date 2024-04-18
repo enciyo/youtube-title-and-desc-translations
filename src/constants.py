270cbb14af85004be332451b141365695de4086e
Mustafa Kılıç <enciyomk61@gmail.com>
2024-04-18 22:08:41 +0300

    reorganized

--- src/constants.py
+++ src/constants.py
@@ -6,7 +6,6 @@ CONST_PASSWORD = os.environ.get("YT_GOOGLE_PASSWORD")
 CONST_WAIT_GOOGLE_LOGIN_TIMEOUT = int(os.environ.get("YT_GOOGLE_LOGIN_TIMEOUT", 200))
 CONST_WAIT_TRANSLATION_TIMEOUT = int(os.environ.get("YT_WAIT_TRANSLATION_TIMEOUT", 5))
 CONST_IS_ENABLE_OTP = os.environ.get("YT_IS_ENABLE_OTP", False) == True
-CONST_WORKSPACE = "~"+os.environ.get("YT_WORKSPACE", os.path.dirname(os.path.realpath(__file__)))
 
 
 
--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from use_cases.click_add_translation import ClickAddTranslations
 from use_cases.add_languages import AddLanguage
 from use_cases.navigate_title_and_desc import NavigateTitleAndDescription
 from use_cases.translate_title_and_desc import TranslateTitleAndDesc
-from logger import append_log_file
+import time
 
 def main():
     """
@@ -37,38 +37,43 @@ def main():
     ])
 
     with SB(uc=True, headless2=True, chromium_arg=chromium_args) as sb:
-        # Open the YouTube Studio URL
-        sb.open(CONST_YOUTUBE_STUDIO_URL)
-        # Perform Google login
-        GoogleLogin().invoke(sb)
-        # Navigate to the subtitle section
-        NavigateToSubtitle().invoke(sb)
-        # Get the list of videos
-        videoCount = len(GetVideos().invoke(sb))
-        for index in range(videoCount):
-            # Navigate to the video detail page
-            NavigateToVideoDetail().invoke(sb,index)
-            # Click on the "Add translations" button for the video
-            while ClickAddTranslations().invoke(sb):
-                # Add the language to the video title and description
-                add_action = AddLanguage().invoke(sb)
-                if add_action["status"] == True:
-                    # Navigate to the title and description section
-                    NavigateTitleAndDescription().invoke(sb,add_action["lang"])
-                    # Translate the title and description
-                    TranslateTitleAndDesc().invoke(sb)
-                else:
-                    # No more languages to add
-                    print(f"No more languages to add for this video ({index}). Moving to the next video.")
-                    break
-            print(f"Completed adding translations for video {index}")
-            sb.go_back()
+        try:
+            # Open the YouTube Studio URL
+            sb.open(CONST_YOUTUBE_STUDIO_URL)
+            # Perform Google login
+            GoogleLogin().invoke(sb)
+            # Navigate to the subtitle section
+            NavigateToSubtitle().invoke(sb)
+            # Get the list of videos
+            videoCount = len(GetVideos().invoke(sb))
+            for index in range(videoCount):
+                # Navigate to the video detail page
+                NavigateToVideoDetail().invoke(sb,index)
+                # Click on the "Add translations" button for the video
+                while ClickAddTranslations().invoke(sb):
+                    # Add the language to the video title and description
+                    add_action = AddLanguage().invoke(sb)
+                    if add_action["status"] == True:
+                        # Navigate to the title and description section
+                        NavigateTitleAndDescription().invoke(sb,add_action["lang"])
+                        # Translate the title and description
+                        TranslateTitleAndDesc().invoke(sb)
+                    else:
+                        # No more languages to add
+                        print(f"No more languages to add for this video ({index}). Moving to the next video.")
+                        break
+                print(f"Completed adding translations for video {index}")
+                sb.go_back()
 
-        sb.wait(5)
+            sb.wait(5)
+        except Exception as e:
+            with open("output/error.log", "w") as f:
+                f.write(f"{time.strftime('%Y-%m-%d %H:%M:%S')} - {str(e)}\n")
+            sb.save_screenshot("output/error.png")
+            sb.quit()
+
+
 
 if __name__ == "__main__":
-    try:
-        main()
-    except Exception as e:
-        append_log_file(f"An error occurred: {str(e)}")
+    main()
 
